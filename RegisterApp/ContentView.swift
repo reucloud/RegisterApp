7f9de0145530f723b137048f77372bcb0645cf1b
reucloud <yuma20040824@icloud.com>
2026-05-17 17:53:03 +0900

    Initial Commit

---
+++ RegisterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RegisterApp
+//
+//  Created by 戸崎悠真 on 2026/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RegisterApp/RegisterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterAppApp.swift
+//  RegisterApp
+//
+//  Created by 戸崎悠真 on 2026/05/17.
+//
+
+import SwiftUI
+
+@main
+struct RegisterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
