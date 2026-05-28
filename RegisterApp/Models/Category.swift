67d42105464ffab9277e5a7c490f6e12933bf542
reucloud <yuma20040824@icloud.com>
2026-05-29 02:06:58 +0900

    DB設計

---
+++ RegisterApp/Models/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  RegisterApp
+//
+//  Created by 戸崎悠真 on 2026/05/29.
+//
+
+import SwiftUI
+
+struct Category: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Category()
+}
---
+++ RegisterApp/Models/Sale.swift
@@ -0,0 +1,18 @@
+//
+//  Sale.swift
+//  RegisterApp
+//
+//  Created by 戸崎悠真 on 2026/05/29.
+//
+
+import SwiftUI
+
+struct Sale: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sale()
+}
